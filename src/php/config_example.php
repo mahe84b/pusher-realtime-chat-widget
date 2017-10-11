3d94c108a24d2c25682a344cb28b1e9ce0903a7b
mahe84b <maheshkumar.budeti@gmail.com>
2017-10-11 10:42:28 +0530

    configs

--- src/php/config_example.php
+++ src/php/config_example.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 
-define('APP_KEY', 'YOUR_APP_KEY');
-define('APP_SECRET', 'YOUR_APP_SECRET');
-define('APP_ID', 'YOUR_APP_ID');
+define('APP_KEY', 'c78f3766dab19de445a2');
+define('APP_SECRET', 'd2b7eb930e62ee8664b2');
+define('APP_ID', '412761');
 ?>
